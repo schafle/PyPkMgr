43c69f09a09c074ba7408ef5ec0f72879acd1b7f
wtpoo <surajchafale@gmail.com>
2016-06-09 00:50:56 -0500

    Basic skeleton of Add, Delete, Update and List Packages is done

--- main.py
+++ main.py
@@ -25,53 +25,81 @@ def exec_with_constraints(cmd, timeout=30, cwd=None):
 		code = process.poll()
 
 		return stdout, stderr, code
+
+def get_installed_packages():
+    import pip
+    phonelist = pip.get_installed_distributions()
+    phonelist = sorted([(i.key, i.version) for i in phonelist])
+    return phonelist
 		
 class PyKkMgr(Tkinter.Tk):
-    def __init__(self,parent):
-        Tkinter.Tk.__init__(self,parent)
-        self.parent = parent
+    def __init__(self):
+        Tkinter.Tk.__init__(self)
+        self.phonelist = get_installed_packages()
         self.initialize()
-
-    def initialize(self):
-        self.grid()
-
-        self.entryVariable = Tkinter.StringVar()
-        self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
-        self.entry.grid(column=0,row=0,sticky='EW')
-        self.entry.bind("<Return>", self.OnPressEnter)
-        self.entryVariable.set(u"Enter text here.")
-
-        button = Tkinter.Button(self,text=u"Click me !",
-                                command=self.OnButtonClick)
-        button.grid(column=1,row=0)
-
-        self.labelVariable = Tkinter.StringVar()
-        label = Tkinter.Label(self,textvariable=self.labelVariable,
-                              anchor="w",fg="white",bg="blue")
-        label.grid(column=0,row=1,columnspan=2,sticky='EW')
-        self.labelVariable.set(u"Hello !")
-
-        self.grid_columnconfigure(0,weight=1)
-        self.resizable(True,False)
-        self.update()
-        self.geometry(self.geometry())       
-        self.entry.focus_set()
-        self.entry.selection_range(0, Tkinter.END)
+        self.setSelect()
 	
-    def OnButtonClick(self):
-        cmd = self.entryVariable.get()
-        #import pdb; pdb.set_trace()
-        stdout, stderr, code = exec_with_constraints(cmd)
-        self.labelVariable.set(stdout)
-        self.entry.focus_set()
-        self.entry.selection_range(0, Tkinter.END)
+    def whichSelected(self) :
+        print "At %s of %d" % (select.curselection(), len(self.phonelist))
+        return int(select.curselection()[0])
 
-    def OnPressEnter(self,event):
-        self.labelVariable.set( self.entryVariable.get())
-        self.entry.focus_set()
-        self.entry.selection_range(0, Tkinter.END)
+    def addEntry(self) :
+        self.phonelist.append ([nameVar.get(), phoneVar.get()])
+        setSelect ()
+
+    def updateEntry(self) :
+        self.phonelist[self.whichSelected()] = [nameVar.get(), phoneVar.get()]
+        setSelect ()
+    
+    def deleteEntry(self) :
+        del self.phonelist[self.whichSelected()]
+        setSelect ()
+    
+    def loadEntry  (self) :
+        name, phone = self.phonelist[self.whichSelected()]
+        nameVar.set(name)
+        phoneVar.set(phone)
+	
+    def initialize(self):
+		global nameVar, phoneVar, select
+
+		frame1 = Tkinter.Frame(self)
+		frame1.pack()
+
+		Tkinter.Label(frame1, text="Name").grid(row=0, column=0, sticky=Tkinter.W)
+		nameVar = Tkinter.StringVar()
+		name = Tkinter.Entry(frame1, textvariable=nameVar)
+		name.grid(row=0, column=1, sticky=Tkinter.W)
+
+		Tkinter.Label(frame1, text="Phone").grid(row=1, column=0, sticky=Tkinter.W)
+		phoneVar= Tkinter.StringVar()
+		phone= Tkinter.Entry(frame1, textvariable=phoneVar)
+		phone.grid(row=1, column=1, sticky=Tkinter.W)
+
+		frame2 = Tkinter.Frame(self)       # Row of buttons
+		frame2.pack()
+		b1 = Tkinter.Button(frame2,text=" Add  ",command=self.addEntry)
+		b2 = Tkinter.Button(frame2,text="Update",command=self.updateEntry)
+		b3 = Tkinter.Button(frame2,text="Delete",command=self.deleteEntry)
+		b4 = Tkinter.Button(frame2,text=" Load ",command=self.loadEntry)
+		b1.pack(side=Tkinter.LEFT); b2.pack(side=Tkinter.LEFT)
+		b3.pack(side=Tkinter.LEFT); b4.pack(side=Tkinter.LEFT)
+
+		frame3 = Tkinter.Frame(self)       # select of names
+		frame3.pack()
+		scroll = Tkinter.Scrollbar(frame3, orient=Tkinter.VERTICAL)
+		select = Tkinter.Listbox(frame3, yscrollcommand=scroll.set, height=6)
+		scroll.config (command=select.yview)
+		scroll.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+		select.pack(side=Tkinter.LEFT,  fill=Tkinter.BOTH, expand=1)
+	
+    def setSelect(self) :
+		self.phonelist.sort()
+		select.delete(0,Tkinter.END)
+		for name,phone in self.phonelist :
+			select.insert (Tkinter.END, name)
 
 if __name__ == "__main__":
-    app = PyKkMgr(None)
+    app = PyKkMgr()
     app.title('Python Package Manager')
     app.mainloop()
